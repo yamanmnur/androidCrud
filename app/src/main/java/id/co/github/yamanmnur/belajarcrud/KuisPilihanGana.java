b24a024d1aaabf1ce7d3f138314b64e9cd66cfcb
yamanmnur <yamanmnur0401@gmail.com>
2018-10-07 12:50:52 +0700

    CRUD Berhasil

---
+++ app/src/main/java/id/co/github/yamanmnur/belajarcrud/KuisPilihanGana.java
@@ -0,0 +1,13 @@
+package id.co.github.yamanmnur.belajarcrud;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KuisPilihanGana extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kuis_pilihan_gana);
+    }
+}
---
+++ app/src/main/java/id/co/github/yamanmnur/belajarcrud/alertDialog.java
@@ -0,0 +1,13 @@
+package id.co.github.yamanmnur.belajarcrud;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class alertDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert_dialog);
+    }
+}
